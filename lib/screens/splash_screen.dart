8db0629f5f15f6951b64491f86c030e3d2c7cf0e
chekib318-stack <chekib318@gmail.com>
2026-07-11 11:28:16 +0100

    Add files via upload

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -108,7 +108,7 @@ class _SplashScreenState extends State<SplashScreen> {
                 borderRadius: BorderRadius.circular(8),
               ),
               child: const Text(
-                'build v0.4.0+4',
+                'build v0.5.0+5',
                 style: TextStyle(color: Colors.white, fontSize: 12, fontWeight: FontWeight.w700),
               ),
             ),
